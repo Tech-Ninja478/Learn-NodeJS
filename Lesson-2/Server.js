5549f001167af87c0db848ceae6e741288ea2c6f
Tech-Ninja478 <dcadityapachpute@gmail.com>
2026-02-12 11:47:56 +0530

    Middleware Understood

--- Lesson-2/Server.js
+++ Lesson-2/Server.js
@@ -14,15 +14,15 @@ console.log(subtract(8, 6));
 console.log(multiply(2, 3));
 console.log(divide(10, 4));
 
-// console.log(os.type());
-// console.log(os.version());
-// console.log(os.homedir());
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
 
-// console.log(__dirname);
-// console.log(__filename);
+console.log(__dirname);
+console.log(__filename);
 
-// console.log(path.dirname(__filename));
-// console.log(path.basename(__filename));
-// console.log(path.extname(__filename));
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
 
-// console.log(path.parse(__filename));
+console.log(path.parse(__filename));
